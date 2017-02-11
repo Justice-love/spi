d31a30be720117e10625afe5788567de771f3096
Justice-love <eddyxu1213@126.com>
2017-02-11 14:58:35 +0800

    modify

--- provider/src/main/java/org/eddy/service/Say.java
+++ provider/src/main/java/org/eddy/service/Say.java
@@ -3,6 +3,8 @@ package org.eddy.service;
 import org.eddy.SayHello;
 import org.kohsuke.MetaInfServices;
 
+import java.util.ServiceLoader;
+
 /**
  * Created by eddy on 2017/2/11.
  */
@@ -11,5 +13,12 @@ public class Say implements SayHello {
 
     public String say() {
         return "hello";
+    }
+
+    public static void main(String[] args) {
+        ServiceLoader<SayHello> services = ServiceLoader.load(SayHello.class);
+        for (SayHello sayHello : services) {
+            sayHello.say();
+        }
     }
 }
